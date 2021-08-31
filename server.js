c47c5055ce0fc1cc537bb4f275544afa3f18e0f6
hrithk <htg2ana@gmail.com>
2021-08-31 20:35:44 +0530

    complete

--- server.js
+++ server.js
@@ -54,11 +54,12 @@ const loginHandler = (req, res) => {
 
   const sigB64 = crypto.createHmac('sha256', time.toString()).update(`${headerB64}.${payloadB64}`).digest("base64");
 
-  tokens[sigB64].time = time;
+  tokens[sigB64] = sigB64;
 
+  console.log(tokens)
   setTimeout(() => {
-    delete tokens[sigB64].time;
-  }, 10);
+    delete tokens[sigB64];
+  }, 20000);
 
   res.send(`${headerB64}.${payloadB64}.${sigB64}`);
 }
@@ -67,15 +68,15 @@ app.post('/login', loginHandler);
 
 
 
-function hasLoggedIn(req, res, next) {
-  console.log("checking if logged in...");
-  if(time != null)
-    next();
-  else
-    res.send("please login");
-}
+// function hasLoggedIn(req, res, next) {
+//   console.log("checking if logged in...");
+//   if(time != null)
+//     next();
+//   else
+//     res.send("please login");
+// }
 
-app.use(hasLoggedIn);
+// app.use(hasLoggedIn);
 
 app.get('/', (req, res) => {
   res.send("hello world!");
@@ -84,9 +85,10 @@ app.get('/', (req, res) => {
 app.post('/verify', (req, res) => {
   // console.log(req.headers);
   console.log(req.headers.authorization);
+  console.log(tokens);
   const [b64Header, b64Payload, b64Sig] = req.headers.authorization.split(".");
 
-  res.send(tokens[JSON.stringify(b64Payload)] && JSON.stringify(tokens[JSON.stringify(b64Payload)]["b64Signature"]) === b64Sig);
+  res.send(tokens[b64Sig] && tokens[b64Sig] === b64Sig ? "true" : "false");
   
 });
 
